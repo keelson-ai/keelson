19ea94d8ae33a637aa91f566f7f2afb9330fecd1
Dima-Othentic <dimar@othentic.xyz>
2026-03-08 20:29:18 +0200

    fix: bugbot comments

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,3 @@ export { useEngine } from './useEngine.js';
 export type { ProbeState, UseEngineResult } from './useEngine.js';
 export { useScan } from './useScan.js';
 export type { ScanState, UseScanResult } from './useScan.js';
-export { useStreamingScan } from './useStreamingScan.js';
-export type { StreamingScanState, UseStreamingScanResult } from './useStreamingScan.js';
